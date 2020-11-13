9f30791adacff07e823dd7ce5b5cfeb7881bf019
Josh K Preston <kanemetal@gmail.com>
2020-11-13 15:46:37 -0700

    'auth_transpile'

--- kanbandoit.server/server/Startup.js
+++ kanbandoit.server/server/Startup.js
@@ -22,11 +22,11 @@ export default class Startup {
         contentSecurityPolicy: {
           directives: {
             defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", `https://${domain}/*`],
-            styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
+            scriptSrc: ["'self'", `https://${domain}/*`, '*'],
+            styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'", '*'],
             imgSrc: ["'self'", '*.githubusercontent.com', '*.blob.core.windows.net'],
             connectSrc: ["'self'", `https://${domain}/oauth/token`, `https://${domain}/userinfo`],
-            fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+            fontSrc: ["'self'", 'https://fonts.gstatic.com', '*'],
             objectSrc: ["'self'"],
             mediaSrc: ["'self'"],
             frameSrc: ["'self'", `https://${domain}`]
